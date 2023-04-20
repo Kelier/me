84d869804ff586dff8a40e60a7a57e8d01c9707a
sometimes <yanjiangnan@bilibili.com>
2023-04-20 17:25:44 +0800

    🖍️: font weight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
+import Me from './modules/Me/main'
 import styles from './page.module.scss'
 
+import localFont from 'next/font/local'
+
+const majorFont = localFont({
+  src: [
+    {
+      path: './assets/fonts/MajorMonoDisplay-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--major-font',
+})
+const supplyFont = localFont({
+  src: [
+    {
+      path: './assets/fonts/PPSupplyMono-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--supply-font',
+})
+
 export default function Home() {
-  return <main className={styles.main}>Hello Me</main>
+  return (
+    <main className={`${styles.main} ${supplyFont.className}`}>
+      <Me></Me>
+    </main>
+  )
 }
