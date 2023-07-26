3a250859774a5e9d84f74ef8cf3d73a3afcfaff3
sometimes <yanjiangnan@bilibili.com>
2023-07-26 17:59:30 +0800

    fix: text loader

--- type.d.ts
+++ type.d.ts
@@ -3,16 +3,3 @@ declare module '*.glb' {
   const value: string
   export default value
 }
-
-declare global {
-  namespace TSX {
-    interface IntrinsicElements {
-      group: any
-      geometry: any
-      lineBasicMaterial: any
-      mesh: any
-      octahedronGeometry: any
-      meshBasicMaterial: any
-    }
-  }
-}
